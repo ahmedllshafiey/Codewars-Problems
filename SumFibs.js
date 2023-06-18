6e7a13e87c1cb0eab51f776e9799661fc69e3eb7
ahmedlldev <ahmedll.shafiey@gmail.com>
2023-06-18 14:46:55 +0300

    NO.35 Problem

---
+++ SumFibs.js
@@ -0,0 +1,16 @@
+function sumFibs() {
+  let sum = 0;
+  let num = 1;
+  let arr = [0,1];
+  for (let i = 1; i < arguments[0]; i++) {
+    num = arr[i - 1] + arr[i];
+    arr.push(num);
+    if(num%2==0){
+      sum = num + sum
+    }
+  }
+
+  return sum;
+}
+
+console.log(sumFibs(5));
