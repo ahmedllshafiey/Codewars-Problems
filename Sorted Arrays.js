c95e2c8bc98ad1b28030369c73e5aa1ee49e6c26
ahmedlldev <ahmedll.shafiey@gmail.com>
2023-06-25 19:07:12 +0300

    N0.44 Problem

---
+++ Sorted Arrays.js
@@ -0,0 +1,19 @@
+function nthSmallest() {
+  let arr = [];
+  let result = [];
+  for (let i = 0; i < arguments.length; i++) {
+    arr.push(arguments[i]);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    for (let k = 0; k < element.length; k++) {
+      result.push(element[k]);
+    }
+  }
+  result.sort(function (e1, e2) {
+    return e1 - e2;
+  });
+
+  return result[arr.slice(-1)[0] - 1];
+}
+console.log(nthSmallest([2, 4, 6, 8, 10, 12], 5));
