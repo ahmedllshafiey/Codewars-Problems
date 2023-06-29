74f1be8ae31f3b102171406153228a6936d2b7a5
ahmedlldev <ahmedll.shafiey@gmail.com>
2023-06-29 14:18:17 +0300

    NO.50 Problem

---
+++ The Maximum And Minimum Difference -- Simple version.js
@@ -0,0 +1,12 @@
+function maxAndMin(arr1, arr2) {
+  let result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let k = 0; k < arr2.length; k++) {
+      let element = Math.abs(arr1[i] - arr2[k]);
+      result.push(element);
+    }
+  }
+  return [Math.max(...result), Math.min(...result)];
+}
+
+console.log(maxAndMin([3, 10, 5], [20, 7, 15, 8]));
