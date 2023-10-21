3252b4a677a1bb01a6ddfd296a666902d8b73688
ahmedlldev <ahmedll.shafiey@gmail.com>
2023-10-21 23:46:58 +0300

    NO.65 Problem

---
+++ Number of trailing zeros of N!.js
@@ -0,0 +1,11 @@
+function zeros(n) {
+  let trailingZeros = 0;
+
+  for (let i = 5; i <= n; i *= 5) {
+    trailingZeros += Math.floor(n / i);
+  }
+
+  return trailingZeros;
+}
+
+console.log(zeros(30));
